326727a01e4630f4c0396ecba366d32227a207e5
谢义学 <xyx_0410@163.com>
2016-10-27 11:35:59 +0800

    [路由] api路由修改

--- y/urls.py
+++ y/urls.py
@@ -23,8 +23,8 @@ router.register('todos', views.TodoViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include(router.urls)),
+    url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 
