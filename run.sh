ec36b37df4a7e63ac7f874438443734484fa5ec2
谢义学 <xyx_0410@163.com>
2016-11-02 11:54:36 +0800

    fix apt-get bug

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ git clone https://github.com/xieyixue/y.git || git pull
 cd /data/y
 ./manage.py makemigrations
 ./manage.py migrate
-./manage.py runserver 0.0.0.0:80 
+./manage.py runserver 0.0.0.0:80
