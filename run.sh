7e272d673ec6f5efe8720b687a5544d7c8beada5
谢义学 <xyx_0410@163.com>
2016-11-02 12:06:43 +0800

    setiings

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ mkdir /data
 cd /data
 git clone https://github.com/xieyixue/y.git || git pull
 cd /data/y
+cp /data/local_settings.py y/local_settings.py
 ./manage.py makemigrations
 ./manage.py migrate
 ./manage.py runserver 0.0.0.0:80
